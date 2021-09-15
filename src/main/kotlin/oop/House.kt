9c3344e3c3b4c5f93f9a81b53d2f0f4f78f4d691
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-15 11:29:38 +0200

    Different class added

--- src/main/kotlin/oop/House.kt
+++ src/main/kotlin/oop/House.kt
@@ -2,10 +2,14 @@ package oop
 
 class House(val height: Double , val color: String, val price: Int) {
     fun print(){
-        println("House [height = $height, color=$color, price=$price]")
+        println("House [height = $height, color = $color, price = $price]")
     }
 }
 
 fun main() {
     val house = House(4.5,"blue",45000)
+    val redHouse = House(5.0,"red",50000)
+
+    house.print()
+    redHouse.print()
 }
