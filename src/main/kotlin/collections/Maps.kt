66b14e4e3d6370bc99e1835af7172c5d5045a79e
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-09-17 09:26:04 +0200

    Maps

---
+++ src/main/kotlin/collections/Maps.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun main() {
+    val namesToAges = mapOf(
+        "Rodney" to 21,
+        "Tafadzwa" to 21
+    )
+    println(namesToAges.keys)
+    println(namesToAges.values)
+    println(namesToAges.entries)
+}
